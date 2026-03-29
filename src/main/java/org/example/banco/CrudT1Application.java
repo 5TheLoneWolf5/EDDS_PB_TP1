b06a452401e69606173c0de323fc9fad4468a8c9
5TheLoneWolf5 <miloalvesnunes@gmail.com>
2026-03-29 10:27:11 -0400

    Working project

--- src/main/java/org/example/banco/CrudT1Application.java
+++ src/main/java/org/example/banco/CrudT1Application.java
@@ -1,13 +1,9 @@
 package org.example.banco;
 
-import org.example.banco.entity.Conta;
 import org.example.banco.service.ContaService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class CrudT1Application implements CommandLineRunner {
@@ -24,43 +20,5 @@ public class CrudT1Application implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-//        consultarConta();
-//        System.out.println("\n");
-//        consultarContas();
-//        System.out.println("\n");
-//        incluirConta();
-//        consultarContas();
-//        System.out.println("\n");
-//        excluirConta();
-//        alterarSaldo();
-//        System.out.println("\n");
-//        consultarContas();
-    }
-
-//    public void alterarSaldo() {
-//        contaService.alterarSaldoConta(2L, 150D);
-//    }
-//
-//    public void consultarConta() {
-//        long id = 1;
-//        Optional<Conta> conta = contaService.consultarContaDb(id);
-//        System.out.println(conta);
-//    }
-//
-//    public void excluirConta() {
-//        long id = 4;
-//        contaService.excluirContaDb(id);
-//    }
-//    public void incluirConta() {
-//    	Conta conta = new Conta("Felipe", 400.00);
-//        contaService.incluirContaDb(conta);
-//    }
-//
-//    public void consultarContas() {
-//        List<Conta> contas = contaService.consultarContasDb();
-//        for (Conta conta : contas) {
-//            System.out.println(conta);
-//        }
-//    }
+    public void run(String... args) {}
 }
